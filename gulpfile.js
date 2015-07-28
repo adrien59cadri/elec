a5f98a95f3a3de7dfd208a9719c2040e68ebfc3f
adrien courdavault <adrien.courdavault@arturia.com>
2015-07-28 10:01:41 +0200

    add some gulp installs and npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 'use strict';
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var install = require("gulp-install");
+
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+
+
+gulp.task('default', function() {
+});
+
+gulp.src(['./bower.json', './package.json'])
+  .pipe(install());
 
 
 gulp.task('lint', function() {
